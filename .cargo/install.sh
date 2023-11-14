92011d746fb8558c78e6711cdd2182bc33ff9cdd
jakubDoka <jakub.doka2@gmail.com>
2023-11-14 19:56:40 +0000

    ''

--- .cargo/install.sh
+++ .cargo/install.sh
@@ -1,9 +1,19 @@
+pacman -S mold rustup
+
 rustup default nightly
 rustup target add \
 	wasm32-unknown-unknown \
 	x86_64-unknown-linux-musl
+rustup component add \
+	rustc-codegen-cranelift-preview \
+	--toolchain nightly
+
 cargo install \
 	trunk \
 	paru \
 	cargo-udeps \
-	cargo-tree
+	cargo-tree \
+	bat \
+	ripgrep
+
+
