be1c9f6e2418664a31afa7775c64e26845644df4
Jakub Doka <jakub.doka2@gmail.com>
2024-12-23 02:02:40 +0100

    configuring git signs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -320,6 +320,16 @@ require('lazy').setup({
       { 'folke/neodev.nvim', opts = {} },
     },
     config = function()
+      for _, method in ipairs { 'textDocument/diagnostic', 'workspace/diagnostic' } do
+        local default_diagnostic_handler = vim.lsp.handlers[method]
+        vim.lsp.handlers[method] = function(err, result, context, config)
+          if err ~= nil and (err.code == -32802 or err.code == -32603) then
+            return
+          end
+          return default_diagnostic_handler(err, result, context, config)
+        end
+      end
+
       vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
         callback = function(event)
