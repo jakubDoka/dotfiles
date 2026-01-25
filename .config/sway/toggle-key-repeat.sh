5c36ccee5dd786e2a994bc3b824ed5d04ab404b2
Jakub Doka <jakub.doka2@gmail.com>
2026-01-25 18:18:26 +0100

    adding repetition toggle

--- .config/sway/toggle-key-repeat.sh
+++ .config/sway/toggle-key-repeat.sh
@@ -5,7 +5,8 @@ RATE=$(swaymsg -t get_inputs | jq -r '
   .[] | select(.type=="keyboard") | .repeat_rate' | head -n1)
 
 if [ "$RATE" = "0" ]; then
-  swaymsg input type:keyboard repeat_delay 300 repeat_rate 50
+  swaymsg input type:keyboard repeat_delay 200
+  swaymsg input type:keyboard repeat_rate 40
 else
   swaymsg input type:keyboard repeat_rate 0
 fi
